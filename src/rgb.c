eb9052e4c576325fe55b1ef243a4ca52431b8fdd
Thomas Smith <thomas@apolloagriculture.com>
2025-05-29 22:37:21 +0200

    Implement LED strip status indicator

---
+++ src/rgb.c
@@ -0,0 +1,53 @@
+#include <zephyr/drivers/led_strip.h>
+
+#include "rgb.h"
+
+struct led_rgb hsb_to_rgb(struct led_hsb hsb) {
+    float r = 0, g = 0, b = 0;
+
+    uint8_t i = hsb.h / 60;
+    float v = hsb.b / ((float)BRT_MAX);
+    float s = hsb.s / ((float)SAT_MAX);
+    float f = hsb.h / ((float)HUE_MAX) * 6 - i;
+    float p = v * (1 - s);
+    float q = v * (1 - f * s);
+    float t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+    case 0:
+        r = v;
+        g = t;
+        b = p;
+        break;
+    case 1:
+        r = q;
+        g = v;
+        b = p;
+        break;
+    case 2:
+        r = p;
+        g = v;
+        b = t;
+        break;
+    case 3:
+        r = p;
+        g = q;
+        b = v;
+        break;
+    case 4:
+        r = t;
+        g = p;
+        b = v;
+        break;
+    case 5:
+        r = v;
+        g = p;
+        b = q;
+        break;
+    }
+
+    struct led_rgb rgb = {r : r * 255, g : g * 255, b : b * 255};
+
+    return rgb;
+}
+
